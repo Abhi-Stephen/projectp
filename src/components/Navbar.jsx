17c8d7aecdbb6d19fcc49bcadfa6603d1655c45d
Abhi-Stephen <abhistephen.rokkam@gmail.com>
2023-12-08 19:58:14 -0600

    upd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,16 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 text-center">
       <div className="flex justify-center space-x-4">
-        <NavLink to="/" exact className="text-white" activeClassName="font-bold">
+        <NavLink to="/" exact className="text-white hover:text-gray-300" activeClassName="font-bold">
           Home
         </NavLink>
-        <NavLink to="/education" className="text-white" activeClassName="font-bold">
+        <NavLink to="/education" className="text-white hover:text-gray-300" activeClassName="font-bold">
           Education
         </NavLink>
-        <NavLink to="/skills" className="text-white" activeClassName="font-bold">
+        <NavLink to="/skills" className="text-white hover:text-gray-300" activeClassName="font-bold">
           Skills
         </NavLink>
-        <NavLink to="/projects" className="text-white" activeClassName="font-bold">
+        <NavLink to="/projects" className="text-white hover:text-gray-300" activeClassName="font-bold">
           Projects
         </NavLink>
       </div>
